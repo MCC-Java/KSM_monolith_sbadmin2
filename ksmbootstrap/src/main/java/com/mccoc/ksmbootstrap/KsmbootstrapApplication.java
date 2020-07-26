1893ea49f2c4ceef936fd1ca5697e738fe7b75e1
Jessi Febria <jfebria23@gmail.com>
2020-07-26 21:01:08 +0700

    KSM with sb admin2

---
+++ ksmbootstrap/src/main/java/com/mccoc/ksmbootstrap/KsmbootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.mccoc.ksmbootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsmbootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsmbootstrapApplication.class, args);
+	}
+
+}
---
+++ ksmbootstrap/src/test/java/com/mccoc/ksmbootstrap/KsmbootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mccoc.ksmbootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsmbootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
